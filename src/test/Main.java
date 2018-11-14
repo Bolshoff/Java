a84b5b37500a18b6efffb2f11f34bbac1125d0d9
black <abolshoff@yandex.ru>
2018-11-14 15:32:40 +0900

    закончил 22 задание

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,23 +1,31 @@
 package test;
 
 import java.io.InputStream;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int a =15;
-     //   hallo();
-       // numbers();
-     //   numbersglam();
-       // numbers2();
-      // sumNumber2();
-       // forest();
-      //  stringToString();
-       // multiplicationTable();
-      //  stringNumber();
-        metNum(15);
+        //  int a =15;
+        //   hallo();
+        // numbers();
+        //   numbersglam();
+        // numbers2();
+        // sumNumber2();
+        // forest();
+        //  stringToString();
+        // multiplicationTable();
+        //  stringNumber();
+        //  metNum(15);
+        //  returner(a);
+        //   returnSquare(5);
+
+        //  sqrt(3, 5);
+        // array();
+        // array1();
+        array2();
     }
 
 
@@ -29,7 +37,7 @@ public class Main {
         System.out.println("Привет " + i);
     }
 
-    static double numbers(){
+    static double numbers() {
 
         double a;
         double b;
@@ -39,11 +47,12 @@ public class Main {
         System.out.println("Введите второе слогаемое");
         Scanner two = new Scanner(System.in);
         b = two.nextDouble();
-        System.out.println(a+b);
-        return a+b;
+        System.out.println(a + b);
+        return a + b;
 
     }
-    static double numbersglam(){
+
+    static double numbersglam() {
 
         double a;
         double b;
@@ -55,36 +64,35 @@ public class Main {
         System.out.println("Введите второе слогаемое");
         Scanner two = new Scanner(System.in);
         b = two.nextDouble();
-        sum = a+b;
-        System.out.println(a + " + " + b + " = " + sum );
+        sum = a + b;
+        System.out.println(a + " + " + b + " = " + sum);
         return sum;
 
     }
-    static double numbers1(){
+
+    static double numbers1() {
 
         double a;
         double b;
         double sum;
 
-            System.out.println("Введите первое слогаемое");
-            Scanner one = new Scanner(System.in);
-            a = one.nextDouble();
-            System.out.println("Введите второе слогаемое");
-            Scanner two = new Scanner(System.in);
-            b = two.nextDouble();
-if (a!=0&&b!=0) {
-    sum = a + b;
+        System.out.println("Введите первое слогаемое");
+        Scanner one = new Scanner(System.in);
+        a = one.nextDouble();
+        System.out.println("Введите второе слогаемое");
+        Scanner two = new Scanner(System.in);
+        b = two.nextDouble();
+        if (a != 0 && b != 0) {
+            sum = a + b;
 
-    System.out.println(a + " + " + b + " = " + sum);
-}
-else {
-    System.out.println(" ZERO Cold");
-}
+            System.out.println(a + " + " + b + " = " + sum);
+        } else {
+            System.out.println(" ZERO Cold");
+        }
 
         return 0;
 
     }
-
 
 
     static double numbers2() {
@@ -92,29 +100,29 @@ else {
         double a;
         double b;
         double sum;
-do {
+        do {
 
-    System.out.println("Введите первое слогаемое");
-    Scanner one = new Scanner(System.in);
-    a = one.nextDouble();
-    System.out.println("Введите второе слогаемое");
-    Scanner two = new Scanner(System.in);
-    b = two.nextDouble();
+            System.out.println("Введите первое слогаемое");
+            Scanner one = new Scanner(System.in);
+            a = one.nextDouble();
+            System.out.println("Введите второе слогаемое");
+            Scanner two = new Scanner(System.in);
+            b = two.nextDouble();
 
-    sum = a + b;
+            sum = a + b;
 
-    System.out.println(a + " + " + b + " = " + sum);
+            System.out.println(a + " + " + b + " = " + sum);
 
-}
-        while (a!=0&&b!=0);
+        }
+        while (a != 0 && b != 0);
         return 0;
     }
 
-    static void   sumNumber(){
+    static void sumNumber() {
         double a;
         double b;
         double c;
-        double result ;
+        double result;
 
         System.out.println("Введите первое число");
         Scanner one = new Scanner(System.in);
@@ -128,14 +136,13 @@ do {
         Scanner three = new Scanner(System.in);
         c = three.nextDouble();
 
-             if (c == 1){
-                 System.out.println(a + b);
+        if (c == 1) {
+            System.out.println(a + b);
         }
-        if (c == 2){
+        if (c == 2) {
 
-            System.out.println(a-b);
-        }
-    else {
+            System.out.println(a - b);
+        } else {
             System.out.println("C не равно 2 или 1");
         }
 
@@ -144,56 +151,56 @@ do {
 
     //8===================================================================
 
-    static void   sumNumber2(){
+    static void sumNumber2() {
         double a;
         double b;
         double c;
-        double result ;
-do {
+        double result;
+        do {
 
-       // while (c<1 || c>4 ){
-    System.out.println("Введите первое число");
-    Scanner one = new Scanner(System.in);
-    a = one.nextDouble();
+            // while (c<1 || c>4 ){
+            System.out.println("Введите первое число");
+            Scanner one = new Scanner(System.in);
+            a = one.nextDouble();
 
-    System.out.println("Введите второе число");
-    Scanner two = new Scanner(System.in);
-    b = two.nextDouble();
+            System.out.println("Введите второе число");
+            Scanner two = new Scanner(System.in);
+            b = two.nextDouble();
 
-    System.out.println("Введите третье число");
-    Scanner three = new Scanner(System.in);
-    c = three.nextDouble();
+            System.out.println("Введите третье число");
+            Scanner three = new Scanner(System.in);
+            c = three.nextDouble();
 
-    if (c == 1) {
-        System.out.println(a + b);
-    }
-    if (c == 2) {
+            if (c == 1) {
+                System.out.println(a + b);
+            }
+            if (c == 2) {
 
-        System.out.println(a - b);
-    }
-    if (c == 3) {
-        System.out.println(a * b);
-    }
-    if (c == 4) {
-        System.out.println(a / b);
-    }
-}
-      while (c<1 || c>4 );
-       System.out.println("C не равно 2 или 1");
+                System.out.println(a - b);
+            }
+            if (c == 3) {
+                System.out.println(a * b);
+            }
+            if (c == 4) {
+                System.out.println(a / b);
+            }
+        }
+        while (c < 1 || c > 4);
+        System.out.println("C не равно 2 или 1");
 
 
     }
 
     //9, 10, 11================================================================
 
-    static  void forest(){
+    static void forest() {
 
-    int a;
-    int b;
-    for(a=1; a<=10; a++) {
-        b=a*a;
-        System.out.println(b);
-    }
+        int a;
+        int b;
+        for (a = 1; a <= 10; a++) {
+            b = a * a;
+            System.out.println(b);
+        }
 
 
     }
@@ -201,13 +208,13 @@ do {
     //12 =========================================================================
 
 
-    static  void stringToString(){
+    static void stringToString() {
 
         int a;
 
-        for(a=1; a<=10; a++) {
-            for (int i = 0; i <=10 ; i++) {
-                System.out.print(i+" ");
+        for (a = 1; a <= 10; a++) {
+            for (int i = 0; i <= 10; i++) {
+                System.out.print(i + " ");
             }
             System.out.println();
         }
@@ -216,14 +223,14 @@ do {
     }
 
     //13 ============================================================================
-    static  void multiplicationTable(   ){
+    static void multiplicationTable() {
 
         int a;
 
-        for(a=1; a<=10; a++) {
+        for (a = 1; a <= 10; a++) {
 
-            for (int i = 1; i <=10 ; i++) {
-                int c=a*i;
+            for (int i = 1; i <= 10; i++) {
+                int c = a * i;
                 System.out.printf("%4d", c);
             }
             System.out.println();
@@ -234,30 +241,110 @@ do {
     }
 
     //14 ============================================================================
-    static void stringNumber (){
-        int a ;
+    static void stringNumber() {
+        int a;
 
-            int i = 1;
+        int i = 1;
 
-            for ( i =1; i <= 100; i++) {
-                System.out.printf("%4d", i); //форматирование текста
-                if(i%10==0){
-                    System.out.println();
-                }
+        for (i = 1; i <= 100; i++) {
+            System.out.printf("%4d", i); //форматирование текста
+            if (i % 10 == 0) {
+                System.out.println();
             }
+        }
 
 
-            }
+    }
 
-
-    static void metNum(int a){
+    //16================================================================================
+    static void metNum(int a) {
 
         System.out.println("Мне передали число " + a);
 
     }
 
 
+    //17==================================================================================
 
+    static int returner(int a) {
+        a = 1;
+        System.out.println(a);
+        return a;
+
+
+    }
+
+    //18=====================================================================================
+    static int returnSquare(int a) {
+        int b = a * a;
+        System.out.println(b);
+        return b;
+    }
+
+    //19 ======================================================================================
+    static double squareSqrt(double a) {
+
+        double b = a * a;
+        return b;
+    }
+
+    static double sqrt(double c, double d) {
+        c = 3;
+        d = 5;
+        double e = squareSqrt(c) + squareSqrt(d);
+        System.out.println(e);
+        System.out.println(Math.sqrt(e));
+        return Math.sqrt(e);
+    }
+
+//20====================================================================
+
+    static void array() {
+
+        int[] a = new int[5];
+        System.out.println("Введите элемент массива: ");
+        Scanner in = new Scanner(System.in);
+        for (int i = 0; i < a.length; i++) {
+            a[i] = in.nextInt();
+        }
+        System.out.println("Введенные элементы: ");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(" " + a[i]);
+        }
+    }
+
+    //21=======================================================================
+    static void array1() {
+
+        int[] a = new int[5];
+        System.out.println("Введите элемент массива: ");
+        Scanner in = new Scanner(System.in);
+        for (int i = 0; i < a.length; i++) {
+            a[i] = in.nextInt();
+        }
+        System.out.println("Введенные элементы: ");
+        for (int i = a.length - 1; i >= 0; i--) {          // выводим элементы массива в обратном порядке;
+            System.out.print(" " + a[i]);
+        }
+    }
+
+    //22============================================================================
+    static void array2() {
+
+        int[] a = new int[5];
+        System.out.println("Введите элемент массива: ");
+        Scanner in = new Scanner(System.in);
+        for (int i = 0; i < a.length; i++) {
+            a[i] = in.nextInt();
+        }
+        System.out.println("Введенные элементы: ");
+        for (int i =0; i < a.length; i++) {
+            if(a[i]==0) {
+                System.out.print(" " + i);
+            }
+        }
+    }
+    //23============================================================================
 
 }
 
@@ -282,12 +369,12 @@ do {
 ++14) Программа, печатающая десять строчек - в первой от 1 до 10, во второй от 11 до 20 и так до десятой, в которой от 91 до 100
 ++15) Программа, в которой есть одна процедура, в которой печатается "Привет" - эта процедура должна вызваться из главной программы.
 ++16) Программа с процедурой, в которую передается в качестве параметра одно число и которая печатает "Мне передали число такое-то". Вызываться эта процедура, понятно, должна с каким-нибудь числом.
-17) Программа с одной функцией, которая ничего не делает а только возвращает число 1. Программа должна печатать значение, возвращенное функцией на экран.
-18 ) Программа с функцией, которая получает в качестве параметра число, а возвращает его квадрат.
-19) Программа в которой будет две функции. Одна для нахождения квадрата числа, которое ей передали в качестве параметра, а вторая чтобы получала два числа и возвращала квадратный корень из суммы их квадратов. Для возведения чисел в квадрат она должна использовать первую функцию, для вычисления квадратного корня - найти стандартную функцию.
-20) Программа, у которой есть массив из пяти чисел. В массив надо ввести пять чисел с клавиатуры (использовать for для прохода по массиву) а потом распечатать эти введенные в массив числа (опять с циклом for).
-21) То же самое, но печатать числа в обратном порядке.
-22) Ввести в массив пять чисел и потом найти среди них 0, если он есть. Напечатать его номер в массиве.
+++17) Программа с одной функцией, которая ничего не делает а только возвращает число 1. Программа должна печатать значение, возвращенное функцией на экран.
+++18 ) Программа с функцией, которая получает в качестве параметра число, а возвращает его квадрат.
+++19) Программа в которой будет две функции. Одна для нахождения квадрата числа, которое ей передали в качестве параметра, а вторая чтобы получала два числа и возвращала квадратный корень из суммы их квадратов. Для возведения чисел в квадрат она должна использовать первую функцию, для вычисления квадратного корня - найти стандартную функцию.
+++20) Программа, у которой есть массив из пяти чисел. В массив надо ввести пять чисел с клавиатуры (использовать for для прохода по массиву) а потом распечатать эти введенные в массив числа (опять с циклом for).
+++21) То же самое, но печатать числа в обратном порядке.
+++22) Ввести в массив пять чисел и потом найти среди них 0, если он есть. Напечатать его номер в массиве.
 23) Ввести пять чисел в массив, потом ввести еще одно число (в дополнительную переменную) и вывести номера всех элементов массива, которые больше этого числа.
 24) Ввести пять чисел в массив и найти самое большое среди них.
 
